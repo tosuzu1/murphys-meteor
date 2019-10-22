9969475681a14cdc86319c6e01d3a2bc4afd6023
Felix Hong <Felix.hong0321@gmail.com>
2019-10-22 10:16:16 -1000

    Finish wod

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -4,17 +4,14 @@ import { Meteor } from 'meteor/meteor';
 import 'semantic-ui-css/semantic.css';
 import { Roles } from 'meteor/alanning:roles';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
-import EditStuff from '../pages/EditStuff';
+import TopMenu from '../components/TopMenu';
+import FooterMenu from '../components/FooterMenu';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
+import Murphys from '../pages/Murphy';
+
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -22,19 +19,15 @@ class App extends React.Component {
     return (
         <Router>
           <div>
-            <NavBar/>
+            <TopMenu/>
             <Switch>
-              <Route exact path="/" component={Landing}/>
+              <Route exact path="/" component={Murphys}/>
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
-              <ProtectedRoute path="/list" component={ListStuff}/>
-              <ProtectedRoute path="/add" component={AddStuff}/>
-              <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-              <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
               <ProtectedRoute path="/signout" component={Signout}/>
               <Route component={NotFound}/>
             </Switch>
-            <Footer/>
+            <FooterMenu/>
           </div>
         </Router>
     );
---
+++ app/imports/ui/pages/Murphy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MiddleMenu from '../components/MiddleMenu';
+
+export default class Murphys extends React.Component {
+  render() {
+    return (
+        <div>
+          <MiddleMenu/>
+        </div>
+    );
+  }
+}
